93bdaf7c3c91515bcb293f8b81fbde970a6ea237
Aztec-Rabbit <ars.xda@gmail.com>
2019-03-24 18:02:10 +0700

    ...

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import app
 import time
 
 def main():
-    for core in range(core = 6):
+    for core in range(6):
         app.client('data\\psiphon\\tunnel-core-{core}\\psiphon-tunnel-core.exe -config data/psiphon/tunnel-core-{core}/config/psiphon-tunnel-core.json', core).start()
 
     time.sleep(60*60*24*30)
--- app/default.py
+++ app/default.py
@@ -5,12 +5,11 @@ from .app import *
 def get_file_names():
     return [
         'database/psiphon.boltdb',
-        'database/psiphon.boltdb.lock',
         'psiphon-tunnel-core.exe'
     ]
 
 def reset_to_default_settings():
-    for core in range(core = 6):
+    for core in range(6):
         for file_name in get_file_names():
             try:
                 path = '/../data/psiphon/tunnel-core-{}/'.format(core)
@@ -20,10 +19,12 @@ def reset_to_default_settings():
     default_settings()
 
 def default_settings():
-    for core in range(core = 6):
+    for core in range(6):
         for file_name in get_file_names():
             try:
                 path = '/../data/psiphon/tunnel-core-{}/'.format(core)
                 open(real_path(path + file_name))
             except:
                 shutil.copyfile(real_path('/default/' + file_name), real_path(path + file_name))
+
+default_settings()
