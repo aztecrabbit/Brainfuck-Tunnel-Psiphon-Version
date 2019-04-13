d257f34c9075ef3f787d2aeb928443ea6aed7f37
Aztec-Rabbit <ars.xda@gmail.com>
2019-04-14 02:10:29 +0700

    ...

--- app/app.py
+++ app/app.py
@@ -19,15 +19,9 @@ def filter_array(array):
 
 def colors(value):
     patterns = {
-        'CC' : '\033[0m',    'BB' : '\033[1m',
-        'D1' : '\033[30;1m', 'D2' : '\033[30;2m',
-        'R1' : '\033[31;1m', 'R2' : '\033[31;2m',
-        'G1' : '\033[32;1m', 'G2' : '\033[32;2m',
-        'Y1' : '\033[33;1m', 'Y2' : '\033[33;2m',
-        'B1' : '\033[34;1m', 'B2' : '\033[34;2m',
-        'P1' : '\033[35;1m', 'P2' : '\033[35;2m',
-        'C1' : '\033[36;1m', 'C2' : '\033[36;2m',
-        'W1' : '\033[37;1m', 'W2' : '\033[37;2m'
+        'R1' : '\033[31;1m', 'G1' : '\033[32;1m',
+        'Y1' : '\033[33;1m', 'P1' : '\033[35;1m',
+        'CC' : '\033[0m'
     }
 
     for code in patterns:
@@ -36,15 +30,16 @@ def colors(value):
     return value
 
 def log(value, status='INFO', color='[G1]'):
-    with lock:
-        print(colors('{color}[{time}] [P1]:: {color}{status} [P1]:: {color}{value}'.format(
-            time=datetime.datetime.now().strftime('%H:%M:%S'),
-            value=value,
-            color=color,
-            status=status
-        )))
+    value = colors('{color}[{time}] [P1]:: {color}{status} [P1]:: {color}{value}'.format(
+        time=datetime.datetime.now().strftime('%H:%M:%S'),
+        value=value,
+        color=color,
+        status=status
+    ))
+    with lock: print(value)
 
 def log_replace(value, status='INFO', color='[G1]'):
+    value = colors('{}{} ({})        \r'.format(color, status, value))
     with lock:
-        sys.stdout.write(colors('{}{} ({})        \r'.format(color, status, value)))
+        sys.stdout.write(value)
         sys.stdout.flush()
--- app/domain_fronting.py
+++ app/domain_fronting.py
@@ -17,8 +17,8 @@ class domain_fronting(threading.Thread):
     def log(self, value, status='INFO', color='[G1]'):
         log(value, status=status, color=color)
 
-    def handler(self):
-        sockets = [self.socket_tunnel, self.socket_client]
+    def handler(self, socket_tunnel, socket_client, buffer_size):
+        sockets = [socket_tunnel, socket_client]
         timeout = 0
         while True:
             timeout += 1
@@ -27,13 +27,13 @@ class domain_fronting(threading.Thread):
             if socket_io:
                 for sock in socket_io:
                     try:
-                        data = sock.recv(self.buffer_size)
+                        data = sock.recv(buffer_size)
                         if not data: break
                         # SENT -> RECEIVED
-                        elif sock is self.socket_client:
-                            self.socket_tunnel.sendall(data)
-                        elif sock is self.socket_tunnel:
-                            self.socket_client.sendall(data)
+                        elif sock is socket_client:
+                            socket_tunnel.sendall(data)
+                        elif sock is socket_tunnel:
+                            socket_client.sendall(data)
                         timeout = 0
                     except: break
             if timeout == 30: break
@@ -45,7 +45,7 @@ class domain_fronting(threading.Thread):
             self.proxy_port = self.proxy_host_port[1] if len(self.proxy_host_port) >= 2 and self.proxy_host_port[1] else '80'
             self.socket_tunnel.connect((str(self.proxy_host), int(self.proxy_port)))
             self.socket_client.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
-            self.handler()
+            self.handler(self.socket_tunnel, self.socket_client, self.buffer_size)
             self.socket_client.close()
             self.socket_tunnel.close()
         except Exception as exception:
